ac373f04dce2d8bc26a087e131c717ffb8471850
gigayulianti <yulianti2@gmail.com>
2026-06-13 17:09:48 +0700

    commit 5

---
+++ ElseIF.cpp
@@ -0,0 +1,14 @@
+
+
+#include < iostream>
+using namespace std;
+float Rata_rata(Float a, float b){
+    return (a+b)/2;
+
+}
+string status_lulus (float r){
+    if (r >= 60)
+return "Lolos":
+    else
+    return "Gagal";
+}
